73bbc01859dc97e4566211992b8caddd3144485f
DiegoTondorf <42287053+DiegoTondorf@users.noreply.github.com>
2025-03-31 16:44:43 -0400

    Update TargetController.cs

--- Assets/Scripts/Controller/TargetController.cs
+++ Assets/Scripts/Controller/TargetController.cs
@@ -130,53 +130,7 @@ public class TargetController : MonoBehaviour
             _icon.enabled = false;
         }
 
-    }
-
-
-
-
-    public Image targetIcon; 
-    public Vector3 target;
-
-    void Start()
-    {
-        targetOriSize = targetIcon.rectTransform.sizeDelta;
-    }
-
-    void Update()
-    {
-        target = SpawningManager.Instance.portalPosition;
-
-        SetTarget(target, targetIcon);
-        
-    }
-
-    void SetTarget(Vector3 _pos, Image _Icon)
-    {
-        if (target != null)
-        {
-            Vector3 screenPos = Camera.main.WorldToScreenPoint(target);
-
-            if (screenPos.z > 0)
-            {
-                screenPos.x = Mathf.Clamp(screenPos.x, 0, Screen.width);
-                screenPos.y = Mathf.Clamp(screenPos.y, 0, Screen.height);
-                targetIcon.transform.position = screenPos;
-                targetIcon.enabled = true;
-
-                float dist = Vector3.Distance(Camera.main.transform.position, target);
-
-                float scale = Mathf.Clamp(100 / dist, 0.25f, 1f); 
-                Vector2 newSize = Vector2.Lerp(targetOriSize*0.25f, targetOriSize, scale);
-
-                targetIcon.rectTransform.sizeDelta = newSize;
-            }
-            else
-            {
-                targetIcon.enabled = false;
-            }
-        }
-
+    
 
     }
 }
