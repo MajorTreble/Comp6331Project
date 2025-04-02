31e06f26d1f1be29d476289e7ff45cb7852a4ba9
DiegoTondorf <42287053+DiegoTondorf@users.noreply.github.com>
2025-04-01 20:53:09 -0400

    Update PlayerReputation.cs

--- Assets/Scripts/Model/JobSystem/PlayerReputation.cs
+++ Assets/Scripts/Model/JobSystem/PlayerReputation.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 using Controller;
 public class PlayerReputation : MonoBehaviour
 {   
@@ -34,6 +35,11 @@ public class PlayerReputation : MonoBehaviour
         */
     }
     
+    public void Start()
+    {
+
+    }
+
     public void ChangeReputation(RepType _type, int _value)
     {
         Reputation rep = reputations.Find(i => i.type == _type);
@@ -43,50 +49,3 @@ public class PlayerReputation : MonoBehaviour
             Debug.LogWarning($"Item of type {_type} not found.");
     }
 }
-
-/*
-namespace Model
-{
-    public enum ReputationStatus
-    {
-        Neutral,
-        Friendly,
-        Enemy
-    }
-
-    [CreateAssetMenu(fileName = "Player Reputation", menuName = "ScriptableObjects/Player Reputation", order = 0)]
-    public class PlayerReputation : ScriptableObject
-    {
-        public float allyReputationThreshold = 70f; // Reputation level required to ally with the player
-
-        public List<Reputation> reputations = new List<Reputation>();
-        public int coins;
-
-        public void Start()
-        {
-            reputations.Add(new Reputation (RepType.Colonial, 0));
-            reputations.Add(new Reputation (RepType.Earth, 0));
-            reputations.Add(new Reputation (RepType.Pirate, 0));
-            reputations.Add(new Reputation (RepType.Self, 0));     
-        }
-
-        public float GetReputation(Faction faction)
-        {
-            return 0.0f;
-        }
-
-        public ReputationStatus GetReputationStatus(Faction faction)
-        {
-            return ReputationStatus.Neutral;
-        }
-
-        public void ChangeReputation(RepType _type, int _value)
-        {
-            Reputation rep = reputations.Find(i => i.type == _type);
-            if (rep != null)
-                rep.ChangeValue(_value);
-            else
-                Debug.LogWarning($"Item of type {_type} not found.");
-        }
-    }
-}*/
