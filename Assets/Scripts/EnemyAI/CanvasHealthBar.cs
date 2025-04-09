dd0dbaeac430944e913d963741b9297da1539202
AsmaShahab55 <asma_shahab33@hotmail.com>
2025-04-09 14:13:19 -0400

    RotationOfHealthBar

--- Assets/Scripts/EnemyAI/CanvasHealthBar.cs
+++ Assets/Scripts/EnemyAI/CanvasHealthBar.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class CanvasHealthBar : MonoBehaviour
 {
     [SerializeField] private Image healthBarSprite;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
 
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
         healthBarSprite.fillAmount = currentHealth / maxHealth;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - cam.transform.position);
     }
 
 }
